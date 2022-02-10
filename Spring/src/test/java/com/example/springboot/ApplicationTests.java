82f652ca2d71658e9f171d1eaa887b2196298954
Iltwats <2012atulsharma@gmail.com>
2022-02-11 00:24:54 +0530

    updated some GET requests and test cases

--- Spring/src/test/java/com/example/springboot/ApplicationTests.java
+++ Spring/src/test/java/com/example/springboot/ApplicationTests.java
@@ -1,13 +1,37 @@
 package com.example.springboot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ApplicationTests {
 
+    @Autowired
+    HelloController helloController;
+
+
     @Test
-    void contextLoads() {
+    void contextLoads() {}
+
+    @Test
+    void version() {
+        assertEquals( "The actual version is 1.0.0", helloController.version());
     }
 
+    @Test
+    void nationsLength() {
+        Integer nationsLength = helloController.getRandomNations().size();
+        assertEquals(10, nationsLength);
+    }
+
+    @Test
+    void currenciesLength() {
+        Integer currenciesLength = helloController.getRandomCurrencies().size();
+        assertEquals(20, currenciesLength);
+
+
+    }
 }
