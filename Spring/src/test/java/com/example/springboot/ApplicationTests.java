84a6435a275f41285dca50fcaabb0dc235e9de6c
Iltwats <2012atulsharma@gmail.com>
2022-02-13 22:32:55 +0530

    Unit test configuration added

--- Spring/src/test/java/com/example/springboot/ApplicationTests.java
+++ Spring/src/test/java/com/example/springboot/ApplicationTests.java
@@ -2,36 +2,37 @@ package com.example.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApplicationTests {
 
     @Autowired
-    HelloController helloController;
+    private HelloController helloController;
 
+    @Autowired
+    private MockMvc mvc;
 
     @Test
-    void contextLoads() {}
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+    }
 
     @Test
     void version() {
         assertEquals( "The actual version is 1.0.0", helloController.version());
     }
 
-    @Test
-    void nationsLength() {
-        Integer nationsLength = helloController.getRandomNations().size();
-        assertEquals(10, nationsLength);
-    }
-
-    @Test
-    void currenciesLength() {
-        Integer currenciesLength = helloController.getRandomCurrencies().size();
-        assertEquals(20, currenciesLength);
-
-
-    }
 }
